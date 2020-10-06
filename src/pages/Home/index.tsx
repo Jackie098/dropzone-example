8d9e2af0029e8c741d60455a737a747424f6d4a7
Carlos A <carlosmiranda19122@gmail.com>
2020-10-06 16:45:15 -0300

    cleaning the project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, Header } from "./styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <Header>Oi</Header>
+    </Container>
+  );
+};
+
+export default Home;
