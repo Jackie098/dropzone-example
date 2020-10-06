be99bc37bd6aca00a06555d8e53a0db6277d34a5
Carlos A <carlosmiranda19122@gmail.com>
2020-10-06 17:58:50 -0300

    Sending to backend: Finished.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Dropzone, {
-  IDropzoneProps,
-  ILayoutProps,
-} from "react-dropzone-uploader";
+import Dropzone, { IDropzoneProps } from "react-dropzone-uploader";
+import { toast, ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.min.css";
 import "react-dropzone-uploader/dist/styles.css";
 
 import { Container, Header, Content, AreaDropzone } from "./styles";
@@ -10,15 +10,26 @@ import { Container, Header, Content, AreaDropzone } from "./styles";
 const Home = () => {
   const getUploadParams: IDropzoneProps["getUploadParams"] = ({ meta }) => {
     return {
-      url: "http://google.com",
+      url: "https://httpbin.org/post",
     };
   };
+
+  // const handleChangeStatus: IDropzoneProps["onChangeStatus"] = (
+  //   { meta, file },
+  //   status
+  // ) => {
+  //   console.log(status, meta, file);
+  // };
 
   const handleChangeStatus: IDropzoneProps["onChangeStatus"] = (
     { meta, file },
     status
   ) => {
-    console.log(status, meta, file);
+    if (status === "headers_received") {
+      toast.success(`Enviado ${file.name} com sucesso!`);
+    } else if (status === "aborted") {
+      toast.error(`${meta.name}, upload failed...`);
+    }
   };
 
   const handleSubmit: IDropzoneProps["onSubmit"] = (files, allFiles) => {
@@ -32,11 +43,21 @@ const Home = () => {
       <Header>Anonimization</Header>
 
       <Content>
+        <ToastContainer />
         <AreaDropzone>
           <Dropzone
             getUploadParams={getUploadParams}
             onChangeStatus={handleChangeStatus}
             onSubmit={handleSubmit}
+            inputContent={(files, { reject }) =>
+              reject ? "Apenas arquivos .csv" : "Arraste aqui"
+            }
+            maxFiles={3}
+            multiple={false}
+            styles={{
+              dropzoneActive: { borderColor: "green" },
+              dropzoneReject: { borderColor: "red" },
+            }}
             accept="text/csv"
           ></Dropzone>
         </AreaDropzone>
