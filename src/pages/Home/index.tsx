3b040976da3e3bdf38f777fcb631bbf221f1b6b5
Carlos A <carlosmiranda19122@gmail.com>
2020-10-06 17:20:27 -0300

    added dropzone and stylized

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,46 @@
 import React from "react";
+import Dropzone, {
+  IDropzoneProps,
+  ILayoutProps,
+} from "react-dropzone-uploader";
+import "react-dropzone-uploader/dist/styles.css";
 
-import { Container, Header } from "./styles";
+import { Container, Header, Content, AreaDropzone } from "./styles";
 
 const Home = () => {
+  const getUploadParams: IDropzoneProps["getUploadParams"] = ({ meta }) => {
+    return {
+      url: "http://google.com",
+    };
+  };
+
+  const handleChangeStatus: IDropzoneProps["onChangeStatus"] = (
+    { meta, file },
+    status
+  ) => {
+    console.log(status, meta, file);
+  };
+
+  const handleSubmit: IDropzoneProps["onSubmit"] = (files, allFiles) => {
+    console.log(files.map((f) => f.meta));
+
+    allFiles.forEach((f) => f.remove());
+  };
+
   return (
     <Container>
-      <Header>Oi</Header>
+      <Header>Anonimization</Header>
+
+      <Content>
+        <AreaDropzone>
+          <Dropzone
+            getUploadParams={getUploadParams}
+            onChangeStatus={handleChangeStatus}
+            onSubmit={handleSubmit}
+            accept="text/csv"
+          ></Dropzone>
+        </AreaDropzone>
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,46 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: var(--background-color-blue);
+  /* width: 100%; */
+  min-height: 750px;
+  display: flex;
+  flex-direction: column;
+`;
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  height: 85px;
+  background: var(--title-color);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 50px;
+  font-weight: bold;
+  color: var(--text-color-blue);
+  border-bottom: 3px solid var(--primary-color);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AreaDropzone = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 100px;
+
+  background: var(--container-color);
+  width: 800px;
+
+  /* div {
+    background: var(--container-color);
+    height: 200px;
+    width: 600px;
+  } */
+`;
